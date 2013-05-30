6fe0c78421ffc9e18c7fb5f19ad200a5cf859247
unknown <jlin@LT126315-JLIN.jomax.paholdings.com>
2013-05-30 14:41:51 -0700

    'initial'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystore::Application.config.secret_token = 'ae3bbbd7bac7d91c44ddc0f70f4a047c8c98cb6012abd3886cbb39cee9e8f752ea284d0ac6ad687ac2b7b44613e47aa01fb8c4a7ab4fb02b80fba9d21c890373'
